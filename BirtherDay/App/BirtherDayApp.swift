24c72260d7c88452877325e2d12b703a6442e616
Rama <gudrms2538@gmail.com>
2025-05-28 11:13:29 +0900

    chore: 프로젝트 폴더링

---
+++ BirtherDay/App/BirtherDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirtherDayApp.swift
+//  BirtherDay
+//
+//  Created by Rama on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BirtherDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
