bd2ef9ab398f824af982f5b304de531ae3783fe5
숩 <realhsb@gmail.com>
2025-06-01 18:13:58 +0900

    chore: 폰트명 수정

--- BirtherDay/Common/Extension/Font+Extension.swift
+++ BirtherDay/Common/Extension/Font+Extension.swift
@@ -11,9 +11,9 @@ import SwiftUI
 
 extension Font {
     // Regular
-    static let r1: Font = .custom("Pretendard-Bold", size: 14)      // 150%
-    static let r2: Font = .custom("Pretendard-Bold", size: 18)      // 150%
-    static let r3: Font = .custom("Pretendard-Bold", size: 20)      // 150%
+    static let r1: Font = .custom("Pretendard-Regular", size: 14)      // 150%
+    static let r2: Font = .custom("Pretendard-Regular", size: 18)      // 150%
+    static let r3: Font = .custom("Pretendard-Regular", size: 20)      // 150%
     
     // Medium
     static let m1: Font = .custom("Pretendard-Medium", size: 16)    // 150%
@@ -26,7 +26,7 @@ extension Font {
     static let sb5: Font = .custom("Pretendard-Semibold", size: 24) // 130%
     
     // Bold
-    static let b1: Font = .custom("Pretendard-bold", size: 14)      // 150%
-    static let b2: Font = .custom("Pretendard-bold", size: 18)      // 150%
-    static let b3: Font = .custom("Pretendard-bold", size: 24)      // 130%
+    static let b1: Font = .custom("Pretendard-Bold", size: 14)      // 150%
+    static let b2: Font = .custom("Pretendard-Bold", size: 18)      // 150%
+    static let b3: Font = .custom("Pretendard-Bold", size: 24)      // 130%
 }
