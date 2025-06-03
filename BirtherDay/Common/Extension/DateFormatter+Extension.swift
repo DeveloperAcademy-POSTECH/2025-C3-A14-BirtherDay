0df8c8e845d902528879d350b4a7aae0ae2e19df
rundo <rundo999@gmial.com>
2025-06-03 19:21:56 +0900

    fix: CouponInfoView 디테일 수정

--- BirtherDay/Common/Extension/DateFormatter+Extension.swift
+++ BirtherDay/Common/Extension/DateFormatter+Extension.swift
@@ -8,9 +8,19 @@
 import Foundation
 
 extension DateFormatter {
+    /// 예: 2024.06.10
     static let expiredDateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy.MM.dd"
         return formatter
     }()
+
+    /// 예: Jun 10, 2024
+    static let englishShortMonthFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, yyyy"
+        formatter.locale = Locale(identifier: "en_US")
+        return formatter
+    }()
 }
+
