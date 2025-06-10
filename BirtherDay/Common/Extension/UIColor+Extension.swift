f4fcfa4fd4dd7b283be74c1d1729f5bbb9065795
Rama <gudrms2538@gmail.com>
2025-06-10 20:02:29 +0900

    feat: 로그인 여부 확인 로직 구현

---
+++ BirtherDay/Common/Extension/UIColor+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extension.swift
+//  BirtherDay
+//
+//  Created by Rama on 6/10/25.
+//
+
+import SwiftUI
+
+struct UIColor_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIColor_Extension()
+}
