5c227536dfdefa747180327e5106d49ea5014f08
rundo <rundo999@gmial.com>
2025-06-11 23:32:26 +0900

    fix: 쿠폰 상세에서 쿠폰 공유해서 받는거까지 확인.

--- BirtherDay/Presentation/MyCoupons/Detail/CouponDetailView.swift
+++ BirtherDay/Presentation/MyCoupons/Detail/CouponDetailView.swift
@@ -169,7 +169,7 @@ struct CouponDetailView: View {
                 .font(.b2)
                 .padding(.top, 16)
             
-            ShareButtons()
+            ShareButtons(couponId: viewModel.selectedCoupon.couponId)  // couponId 전달
                 .padding(.top, 24)
         }
     }
